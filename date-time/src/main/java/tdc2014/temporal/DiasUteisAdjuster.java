cbbe3bf4f325663f043005b03e0b402bd2a211f4
Michel Graciano <michel.graciano@gmail.com>
2014-11-22 15:29:18 -0200

    Criado Javadoc e aprimorado nome de variáveis

--- date-time/src/main/java/tdc2014/temporal/DiasUteisAdjuster.java
+++ date-time/src/main/java/tdc2014/temporal/DiasUteisAdjuster.java
@@ -5,18 +5,38 @@ import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.util.stream.Stream;
 
+/**
+ * Um {@link TemporalAdjuster} responsável por ajustar uma {@link Temporal data} informada para o dia útil mais próximo.
+ * Caso a data informada seja um dia útil, ela mesma será retornada.
+ *
+ * É possível ainda determinar quantos dias úteis devem ser avançados. Por exemplo, para obter-se o 5o dia útil a partir
+ * de uma data, o construtor {@link DiasUteisAdjuster#DiasUteisAdjuster(long)} deve ser utilizado informando o valor
+ * {@code 5}.
+ */
 public class DiasUteisAdjuster implements TemporalAdjuster {
 
-    private long diasAjuste;
+    private long quantidadeDias;
 
     private final DiaUtilQuery diaUtilQuery;
 
+    /**
+     * Cria um {@link TemporalAdjuster} responsável por ajustar uma {@link Temporal data} informada para o dia útil mais
+     * próximo. Caso a data informada seja um dia útil, ela mesma será retornada.
+     */
     public DiasUteisAdjuster() {
         this(1);
     }
 
-    public DiasUteisAdjuster(long diasAjuste) {
-        this.diasAjuste = diasAjuste;
+    /**
+     * Cria um {@link TemporalAdjuster} responsável por ajustar uma {@link Temporal data} informada para o dia útil mais
+     * próximo, dado a {@link DiasUteisAdjuster#DiasUteisAdjuster(long) quantidadede dias} informada.
+     *
+     * Para obter o 5o dia util a partir de uma data, informe {@code 5} como parâmetro.
+     *
+     * @param quantidadeDias
+     */
+    public DiasUteisAdjuster(final long quantidadeDias) {
+        this.quantidadeDias = quantidadeDias;
         this.diaUtilQuery = new DiaUtilQuery(FeriadosFactory.create());
     }
 
@@ -25,7 +45,7 @@ public class DiasUteisAdjuster implements TemporalAdjuster {
         final LocalDate from = LocalDate.from(temporal);
         return Stream.iterate(from, day -> day.plusDays(1))
                 .filter(day -> day.query(diaUtilQuery))
-                .skip(diasAjuste - 1)
+                .skip(quantidadeDias - 1)
                 .findFirst().get();
     }
 }
