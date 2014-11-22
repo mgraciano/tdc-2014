973a6b8a1865495f8010943099b4f018ae29fcce
Michel Graciano <michel.graciano@gmail.com>
2014-11-22 16:01:27 -0200

    Corrigido nome de testes e reescrito alguns testes com lambdas

--- date-time/src/main/java/tdc2014/demo/diversos/Exemplos2.java
+++ date-time/src/main/java/tdc2014/demo/diversos/Exemplos2.java
@@ -15,30 +15,21 @@ public class Exemplos2 {
     public long getTotalMesesAteONatal() {
         LocalDate dataAtual = LocalDate.now();
         LocalDate natal = LocalDate.of(2014, Month.DECEMBER, 25);
-        
+
         return dataAtual.until(natal).toTotalMonths();
     }
 
-    public long getTotalDiasAteOAnoNovo() {
+    public long getMinutosDiasAteOAnoNovo() {
         LocalDateTime dataAtual = LocalDateTime.now();
         LocalDateTime anoNovo = dataAtual.with(TemporalAdjusters.firstDayOfNextYear());
         return Duration.between(dataAtual, anoNovo).toMinutes();
     }
 
     public List<DayOfWeek> getUltimoDiaDosMesesEmUmAno(int valorAno) {
-        Year ano = Year.of(valorAno);
-        
-//        List<DayOfWeek> diasDaSemana = new ArrayList<>();
-//        for(Month mes : Month.values()){
-//            DayOfWeek diaDaSemana =  ano.atMonth(mes).atEndOfMonth().getDayOfWeek();
-//            
-//            diasDaSemana.add(diaDaSemana);
-//        }
-        
-//        return diasDaSemana;        
-        
+        final Year ano = Year.of(valorAno);
+
         return Stream.of(Month.values())
-              .<DayOfWeek>map((mes) -> ano.atMonth(mes).atEndOfMonth().getDayOfWeek())
-              .collect(Collectors.toList());        
+                .map((mes) -> ano.atMonth(mes).atEndOfMonth().getDayOfWeek())
+                .collect(Collectors.toList());
     }
 }
